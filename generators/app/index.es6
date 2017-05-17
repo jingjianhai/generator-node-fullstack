5585faa81ca2207b114db60863e022799229a5ae
Tony Stark <swzyocowboy@icloud.com>
2017-05-17 17:25:38 +0800

    feat:(新功能): 选择许可证

--- generators/app/index.es6
+++ generators/app/index.es6
@@ -4,22 +4,120 @@ import Generator from 'yeoman-generator';
 import chalk from 'chalk';
 import yosay from 'yosay';
 import forEach from 'lodash/forEach';
+import assign from 'lodash/assign';
+import gitConfig from 'git-config';
+import glob from 'glob';
+
+let licenses = [
+  {
+    name: 'Apache 2.0',
+    value: 'Apache-2.0'
+  }, {
+    name: 'MIT',
+    value: 'MIT'
+  }, {
+    name: 'Mozilla Public License 2.0',
+    value: 'MPL-2.0'
+  }, {
+    name: 'BSD 2-Clause (FreeBSD) License',
+    value: 'BSD-2-Clause-FreeBSD'
+  }, {
+    name: 'BSD 3-Clause (NewBSD) License',
+    value: 'BSD-3-Clause'
+  }, {
+    name: 'Internet Systems Consortium (ISC) License',
+    value: 'ISC'
+  }, {
+    name: 'GNU AGPL 3.0',
+    value: 'AGPL-3.0'
+  }, {
+    name: 'GNU GPL 3.0',
+    value: 'GPL-3.0'
+  }, {
+    name: 'GNU LGPL 3.0',
+    value: 'LGPL-3.0'
+  }, {
+    name: 'Unlicense',
+    value: 'unlicense'
+  }, {
+    name: 'No License (Copyrighted)',
+    value: 'nolicense'
+  }
+];
 
 class NodeFullstack extends Generator {
   constructor(args, opts) {
     super(args, opts);
+
     this.argument('appname', {
       type: String,
       required: false
     });
+
+    this.option('name', {
+      type: String,
+      desc: '许可证拥有者',
+      required: false
+    });
+
+    this.option('email', {
+      type: String,
+      desc: '许可证拥有者的电子邮箱',
+      required: false
+    });
+
+    this.option('website', {
+      type: String,
+      desc: '许可证拥有者的网站',
+      required: false
+    });
+
+    this.option('year', {
+      type: String,
+      desc: '许可证有效年限',
+      required: false,
+      defaults: (new Date()).getFullYear()
+    });
+
+    this.option('licensePrompt', {
+      type: String,
+      desc: '许可证提示',
+      defaults: '你想要使用哪种许可证？',
+      hide: true,
+      required: false
+    });
+
+    this.option('defaultLicense', {
+      type: String,
+      desc: '默认许可证',
+      required: false
+    });
+
+    this.option('license', {
+      type: String,
+      desc: '选择一个许可证后就不会再有类似提示了',
+      required: false
+    });
+
+    this.option('output', {
+      type: String,
+      desc: '为即将生成的许可证设置输出文件（名，包括后缀）',
+      required: false,
+      defaults: 'LICENSE'
+    });
+  }
+  initializing() {
+    this.props = {};
+    this.gitc = gitConfig.sync();
+    this.gitc.user = this.gitc.user || {};
   }
   prompting() {
     let generator = chalk.red('Node 全栈');
     this.log(yosay(`欢迎使用 ${generator} 生成器`));
 
-    // TODO: 输入目前确认的开发者信息，用于填充 `.mailmap`、`package.json` 等文件
-    // TODO: 选择许可证
-    // TODO: 设定不同环境下服务器、数据库等配置
+    // TODO: 新功能，输入目前确认的开发者信息，用于填充 `.mailmap`、`package.json` 等文件
+    // TODO: 新功能，设定不同环境下服务器、数据库等配置
+    // TODO: 修复，生成的 `doc/deployment.md` 文件内密码字样出现转义现象
     const prompts = [{
       type: 'input',
       name: 'iptProjectName',
@@ -37,28 +135,76 @@ class NodeFullstack extends Generator {
       name: 'iptRedisPassword',
       message: '设置 redis 数据库的访问密码',
       default: 'cnDaNdAn!@#$&13679'
+    }, {
+      name: 'name',
+      message: '你的姓名',
+      default: this.options.name || this.gitc.user.name,
+      when: this.options.name === null || this.options.name === undefined
+    }, {
+      name: 'email',
+      message: '你的电子邮箱 (可选):',
+      default: this.options.email || this.gitc.user.email,
+      when: !this.options.email
+    }, {
+      name: 'website',
+      message: '你的网站 (可选):',
+      default: this.options.website,
+      when: !this.options.website
+    }, {
+      type: 'list',
+      name: 'license',
+      message: this.options.licensePrompt,
+      default: this.options.defaultLicense,
+      when: !this.options.license ||
+              licenses.find(x => x.value === this.options.license) === undefined,
+      choices: licenses
     }];
 
     return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
+      this.props = assign({
+        name: this.options.name,
+        email: this.options.email,
+        website: this.options.website,
+        license: this.options.license,
+        year: this.options.license
+      }, props);
     });
   }
 
   writing() {
     this._copyTpl();
+
     this._copy();
   }
 
   _copyTpl() {
+    let filename = this.props.license + '.txt';
+    let author = this.props.name.trim();
+    if (this.props.email) {
+      author += ' <' + this.props.email.trim() + '>';
+    }
+    if (this.props.website) {
+      author += ' (' + this.props.website.trim() + ')';
+    }
+
     let passed = {
       redisPassword: this.props.iptRedisPassword,
       projectDescription: this.props.iptProjectDescription,
-      projectName: this.props.iptProjectName
+      projectName: this.props.iptProjectName,
+      year: this.options.year,
+      author: author
     };
 
+    let _path = 'license/' + filename;
+
     this.fs.copyTpl(
-      this.templatePath(),
+      this.templatePath(_path),
+      this.destinationPath(this.options.output),
+      passed
+    );
+
+    this.fs.copyTpl(
+      glob.sync(this.templatePath('./*'), {dot: true, nodir: true}),
       this.destinationPath(),
       passed
     );
@@ -68,6 +214,22 @@ class NodeFullstack extends Generator {
       this.destinationPath('doc'),
       passed
     );
+
+    if (!this.fs.exists(this.destinationPath('package.json'))) {
+      return;
+    }
+
+    let pkg = this.fs.readJSON(this.destinationPath('package.json'), {});
+    pkg.license = this.props.license;
+
+    // We don't want users to publish their module to NPM if they copyrighted
+    // their content.
+    if (this.props.license === 'nolicense') {
+      delete pkg.license;
+      pkg.private = true;
+    }
+
+    this.fs.writeJSON(this.destinationPath('package.json'), pkg);
   }
 
   _copy() {
@@ -103,3 +265,4 @@ class NodeFullstack extends Generator {
 }
 
 module.exports = NodeFullstack;
+module.exports.licenses = licenses;
--- generators/app/index.js
+++ generators/app/index.js
@@ -18,6 +18,18 @@ var _forEach = require('lodash/forEach');
 
 var _forEach2 = _interopRequireDefault(_forEach);
 
+var _assign = require('lodash/assign');
+
+var _assign2 = _interopRequireDefault(_assign);
+
+var _gitConfig = require('git-config');
+
+var _gitConfig2 = _interopRequireDefault(_gitConfig);
+
+var _glob = require('glob');
+
+var _glob2 = _interopRequireDefault(_glob);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -26,22 +38,116 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var MyGenerator = function (_Generator) {
-  _inherits(MyGenerator, _Generator);
+var licenses = [{
+  name: 'Apache 2.0',
+  value: 'Apache-2.0'
+}, {
+  name: 'MIT',
+  value: 'MIT'
+}, {
+  name: 'Mozilla Public License 2.0',
+  value: 'MPL-2.0'
+}, {
+  name: 'BSD 2-Clause (FreeBSD) License',
+  value: 'BSD-2-Clause-FreeBSD'
+}, {
+  name: 'BSD 3-Clause (NewBSD) License',
+  value: 'BSD-3-Clause'
+}, {
+  name: 'Internet Systems Consortium (ISC) License',
+  value: 'ISC'
+}, {
+  name: 'GNU AGPL 3.0',
+  value: 'AGPL-3.0'
+}, {
+  name: 'GNU GPL 3.0',
+  value: 'GPL-3.0'
+}, {
+  name: 'GNU LGPL 3.0',
+  value: 'LGPL-3.0'
+}, {
+  name: 'Unlicense',
+  value: 'unlicense'
+}, {
+  name: 'No License (Copyrighted)',
+  value: 'nolicense'
+}];
 
-  function MyGenerator(args, opts) {
-    _classCallCheck(this, MyGenerator);
+var NodeFullstack = function (_Generator) {
+  _inherits(NodeFullstack, _Generator);
 
-    var _this = _possibleConstructorReturn(this, (MyGenerator.__proto__ || Object.getPrototypeOf(MyGenerator)).call(this, args, opts));
+  function NodeFullstack(args, opts) {
+    _classCallCheck(this, NodeFullstack);
+
+    var _this = _possibleConstructorReturn(this, (NodeFullstack.__proto__ || Object.getPrototypeOf(NodeFullstack)).call(this, args, opts));
 
     _this.argument('appname', {
       type: String,
       required: false
     });
+
+    _this.option('name', {
+      type: String,
+      desc: '许可证拥有者',
+      required: false
+    });
+
+    _this.option('email', {
+      type: String,
+      desc: '许可证拥有者的电子邮箱',
+      required: false
+    });
+
+    _this.option('website', {
+      type: String,
+      desc: '许可证拥有者的网站',
+      required: false
+    });
+
+    _this.option('year', {
+      type: String,
+      desc: '许可证有效年限',
+      required: false,
+      defaults: new Date().getFullYear()
+    });
+
+    _this.option('licensePrompt', {
+      type: String,
+      desc: '许可证提示',
+      defaults: '你想要使用哪种许可证？',
+      hide: true,
+      required: false
+    });
+
+    _this.option('defaultLicense', {
+      type: String,
+      desc: '默认许可证',
+      required: false
+    });
+
+    _this.option('license', {
+      type: String,
+      desc: '选择一个许可证后就不会再有类似提示了',
+      required: false
+    });
+
+    _this.option('output', {
+      type: String,
+      desc: '为即将生成的许可证设置输出文件（名，包括后缀）',
+      required: false,
+      defaults: 'LICENSE'
+    });
     return _this;
   }
 
-  _createClass(MyGenerator, [{
+  _createClass(NodeFullstack, [{
+    key: 'initializing',
+    value: function initializing() {
+      this.props = {};
+      this.gitc = _gitConfig2['default'].sync();
+      this.gitc.user = this.gitc.user || {};
+    }
+  }, {
     key: 'prompting',
     value: function prompting() {
       var _this2 = this;
@@ -50,7 +156,6 @@ var MyGenerator = function (_Generator) {
       this.log((0, _yosay2['default'])('\u6B22\u8FCE\u4F7F\u7528 ' + generator + ' \u751F\u6210\u5668'));
 
       // TODO: 输入目前确认的开发者信息，用于填充 `.mailmap`、`package.json` 等文件
-      // TODO: 选择许可证
       // TODO: 设定不同环境下服务器、数据库等配置
       var prompts = [{
         type: 'input',
@@ -69,31 +174,92 @@ var MyGenerator = function (_Generator) {
         name: 'iptRedisPassword',
         message: '设置 redis 数据库的访问密码',
         'default': 'cnDaNdAn!@#$&13679'
+      }, {
+        name: 'name',
+        message: '你的姓名',
+        'default': this.options.name || this.gitc.user.name,
+        when: this.options.name === null || this.options.name === undefined
+      }, {
+        name: 'email',
+        message: '你的电子邮箱 (可选):',
+        'default': this.options.email || this.gitc.user.email,
+        when: !this.options.email
+      }, {
+        name: 'website',
+        message: '你的网站 (可选):',
+        'default': this.options.website,
+        when: !this.options.website
+      }, {
+        type: 'list',
+        name: 'license',
+        message: this.options.licensePrompt,
+        'default': this.options.defaultLicense,
+        when: !this.options.license || licenses.find(function (x) {
+          return x.value === _this2.options.license;
+        }) === undefined,
+        choices: licenses
       }];
 
       return this.prompt(prompts).then(function (props) {
-        // To access props later use this.props.someAnswer;
-        _this2.props = props;
+        _this2.props = (0, _assign2['default'])({
+          name: _this2.options.name,
+          email: _this2.options.email,
+          website: _this2.options.website,
+          license: _this2.options.license,
+          year: _this2.options.license
+        }, props);
       });
     }
   }, {
     key: 'writing',
     value: function writing() {
       this._copyTpl();
+
       this._copy();
     }
   }, {
     key: '_copyTpl',
     value: function _copyTpl() {
+      var filename = this.props.license + '.txt';
+      var author = this.props.name.trim();
+      if (this.props.email) {
+        author += ' <' + this.props.email.trim() + '>';
+      }
+      if (this.props.website) {
+        author += ' (' + this.props.website.trim() + ')';
+      }
+
       var passed = {
         redisPassword: this.props.iptRedisPassword,
         projectDescription: this.props.iptProjectDescription,
-        projectName: this.props.iptProjectName
+        projectName: this.props.iptProjectName,
+        year: this.options.year,
+        author: author
       };
 
-      this.fs.copyTpl(this.templatePath(), this.destinationPath(), passed);
+      var _path = 'license/' + filename;
+
+      this.fs.copyTpl(this.templatePath(_path), this.destinationPath(this.options.output), passed);
+
+      this.fs.copyTpl(_glob2['default'].sync(this.templatePath('./*'), { dot: true, nodir: true }), this.destinationPath(), passed);
 
       this.fs.copyTpl(this.templatePath('doc'), this.destinationPath('doc'), passed);
+
+      if (!this.fs.exists(this.destinationPath('package.json'))) {
+        return;
+      }
+
+      var pkg = this.fs.readJSON(this.destinationPath('package.json'), {});
+      pkg.license = this.props.license;
+
+      // We don't want users to publish their module to NPM if they copyrighted
+      // their content.
+      if (this.props.license === 'nolicense') {
+        delete pkg.license;
+        pkg['private'] = true;
+      }
+
+      this.fs.writeJSON(this.destinationPath('package.json'), pkg);
     }
   }, {
     key: '_copy',
@@ -116,7 +282,8 @@ var MyGenerator = function (_Generator) {
     }
   }]);
 
-  return MyGenerator;
+  return NodeFullstack;
 }(_yeomanGenerator2['default']);
 
-module.exports = MyGenerator;
+module.exports = NodeFullstack;
+module.exports.licenses = licenses;
